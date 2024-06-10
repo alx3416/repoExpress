c6c60ba8dfba51f5ff8ba57b71b9ef2a7f04a690
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-06-10 13:28:01 -0600

    define getCircleArea

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # hola mundo en python
+import math
 
 def myFunction():
     print("Hola mundo")
@@ -8,5 +9,9 @@ def getSquareArea(side):
     return side ** 2
 
 
+def getCircleArea(radius):
+    return math.pi * (radius ** 2)
+
 myFunction()
 print(getSquareArea(3))
+print(getCircleArea(10))
