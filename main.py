9f2d020711029ff8151ee359b79acca42a286af6
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-06-10 13:47:59 -0600

    add get-triangle-area

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ def getSquareArea(side):
 def getCircleArea(radius):
     return math.pi * (radius ** 2)
 
+
+def getTriangleArea(side1, side2):
+    return (side1 * side2) / 2.0
+
+
 myFunction()
 print(getSquareArea(3))
 print(getCircleArea(20))
+print(getTriangleArea(5, 7))
